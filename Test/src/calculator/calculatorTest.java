6a8c8597e84c0945aba8ff58189a80e00460474a
gksma <dhfz1794@naver.com>
2025-05-20 10:09:26 +0900

    first commit

---
+++ Test/src/calculator/calculatorTest.java
@@ -0,0 +1,27 @@
+package calculator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class calculatorTest {
+	
+	calculator calc;
+
+	@BeforeEach
+	void setUp() {
+		calc = new calculator();
+	}
+
+	@Test
+	void testAddIntInt() {
+		assertEquals(10, calc.add(3, 7));
+	}
+
+	@Test
+	void testAddStringString() {
+		assertEquals("apple", calc.add("app", "le"));
+	}
+
+}
---
+++ Test/src/student/StudentManagerTest.java
@@ -0,0 +1,47 @@
+package student;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class StudentManagerTest {
+	
+	StudentManager manage;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		manage = new StudentManager();
+	}
+
+	@Test
+	void testAddStudent() {
+		manage.addStudent("조한흠");
+		assertTrue(manage.hasStudent("조한흠"));
+	}
+
+	@Test
+	void testRemoveStudent() {
+		manage.addStudent("조한흠");
+		manage.removeStudent("조한흠");
+		assertFalse(manage.hasStudent("조한흠"));
+	}
+	
+	@Test
+	void testAddException() {
+		manage.addStudent("조한흠");
+		assertThrows(IllegalArgumentException.class, () -> {
+			manage.addStudent("조");
+		});
+	}
+
+	@Test
+	void testRemoveException() {
+		manage.addStudent("조한흠");
+		manage.removeStudent("조한흠");
+		assertThrows(IllegalArgumentException.class, () -> {
+			manage.removeStudent("조한흠");
+		});
+	}
+
+}
